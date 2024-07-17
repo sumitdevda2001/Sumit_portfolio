94890c8f3ed6aed7e3537517d8d2c11854953206
Sumit Devda <sumitdevda2001@gmail.com>
2024-07-17 11:34:01 +0530

    update resume in page

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sumitdevda2001",
   resume:
-    "https://drive.google.com/file/d/1K2__cQfYBwLWBpvQhkfQgDCcubZsWBC1/view?usp=sharing",
+    "https://drive.google.com/file/d/1mo8rpwFQd5MkQOVE3CyoAl9IezUvOzDb/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sumit-devda-5575791a1/",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
